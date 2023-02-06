f0825924733f27f4652c22dc05a335d4b2130404
Niram11 <dominikwidera2@gmail.com>
2023-02-06 11:06:16 +0100

    dodanie git ignore, dodanie folderu templates, dodanie templatki dla strony list,

--- server.py
+++ server.py
@@ -3,7 +3,8 @@ This layer contains logic related to Flask, such as server,
 routes, request handling, session, and so on. 
 This is the only file to be imported from Flask.
 """
-from flask import Flask
+from data_manager import data_crud
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
@@ -12,6 +13,17 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route('/list')
+def list_questions():
+    #user_stories = data_crud.read_data() #do dodania nazwa pliku
+
+
+    return render_template('list.html')
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(
+        host='0.0.0.0',
+        port=5000,
+        debug=True,
+    )
