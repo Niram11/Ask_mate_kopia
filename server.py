766e2453f39d2e8051e3dea83a80553aa5ed8561
Niram11 <dominikwidera2@gmail.com>
2023-02-06 16:14:52 +0100

    dodanie route dla wszystkich zakladek

--- server.py
+++ server.py
@@ -19,6 +19,41 @@ def list_questions():
     matrix = data.sort_matrix_by_column_with_headers(a,column=3 )
     return render_template('list.html', questions = matrix)
 
+@app.route('/add-question')
+def add_question():
+    return 'add_question'
+
+app.route('/question/')
+def question():
+    return 'question'
+
+app.route('/add-question')
+def add_question():
+    return 'add_question'
+
+app.route('/question/delete')
+def question_delete():
+    return 'question_delete'
+
+app.route('/question/edit')
+def question_edit():
+    return 'question_edit'
+
+app.route('/question/new-answer')
+def new_answer():
+    return 'new_answer'
+
+app.route('/answer/delete')
+def answer_delete():
+    return 'answer_delete'
+
+app.route('/question/vote-up')
+def vote_up():
+    return 'vote_up'
+
+app.route('/question/vote-down')
+def vote_down():
+    return 'vote_down'
 
 if __name__ == "__main__":
     app.run(
